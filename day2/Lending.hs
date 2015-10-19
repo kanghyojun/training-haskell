1ea86a2893ca3ce2c913d9a0915929b7f1987ca2
Hyojun Kang <admire9@gmail.com>
2015-10-20 00:54:33 +0900

    p62

---
+++ day2/Lending.hs
@@ -0,0 +1,9 @@
+type Money = Int
+
+lend :: Money -> Money -> Maybe Money
+
+lend amount balance = let reserve = 100
+                          newBalance = balance - amount
+                      in if balance < reserve
+                         then Nothing
+                         else Just newBalance
